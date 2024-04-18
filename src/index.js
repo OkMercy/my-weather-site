59c57559a98d849747d656e36d50c68d3519622c
OkMercy <144592861+OkMercy@users.noreply.github.com>
2024-04-18 12:57:10 +0600

    Added axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 function handleSearchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-
   let city = document.querySelector("#city");
+
   city.innerHTML = searchInput.value;
 }
 
+function searchLocation(city) {
+  let apiKey = "60o969fa37bedcb0t842da02274e8c33";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+
 searchFormElement.addEventListener("submit", handleSearchCity);
