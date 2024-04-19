5489bb08d184d03568ae26d97e6faeaae84f406c
OkMercy <144592861+OkMercy@users.noreply.github.com>
2024-04-19 19:44:54 +0600

    Added functionality using API
    Updated hard code to real time data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-function handleSearchCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input");
+function updateWeather(response) {
+  let temperature = response.data.temperature.current;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(temperature);
+
   let city = document.querySelector("#city");
 
-  city.innerHTML = searchInput.value;
+  city.innerHTML = response.data.city;
 }
 
 function searchLocation(city) {
   let apiKey = "60o969fa37bedcb0t842da02274e8c33";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function handleSearchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchLocation(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchCity);
+
+searchLocation("Lagos");
